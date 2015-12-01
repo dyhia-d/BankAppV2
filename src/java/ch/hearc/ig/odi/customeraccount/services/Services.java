4643f99686b4fbfbc8fb094e7fc77e3cdb678de5
Dyhia Dib <dyhia.dib@he-arc.ch>
2015-12-01 22:55:24 +0100

    Fin série 8

--- src/java/ch/hearc/ig/odi/customeraccount/services/Services.java
+++ src/java/ch/hearc/ig/odi/customeraccount/services/Services.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ch.hearc.ig.odi.customeraccount.services;
 
-/**
- *
- * @author alexandr.ducommun
- */
 import ch.hearc.ig.odi.customeraccount.business.Bank;
 import ch.hearc.ig.odi.customeraccount.business.Customer;
 import java.io.Serializable;
