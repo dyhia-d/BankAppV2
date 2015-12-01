4e46532ea6d5fce11147f2ee01643deca5bdaf33
Dyhia Dib <dyhia.dib@he-arc.ch>
2015-12-01 22:54:03 +0100

    Revert "Fin série 8"
    This reverts commit ae6909cd0257ea2a663677f83122fd81f54864cb.

--- src/java/ch/hearc/ig/odi/customeraccount/services/Services.java
+++ src/java/ch/hearc/ig/odi/customeraccount/services/Services.java
@@ -1,5 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package ch.hearc.ig.odi.customeraccount.services;
 
+/**
+ *
+ * @author alexandr.ducommun
+ */
 import ch.hearc.ig.odi.customeraccount.business.Bank;
 import ch.hearc.ig.odi.customeraccount.business.Customer;
 import java.io.Serializable;
