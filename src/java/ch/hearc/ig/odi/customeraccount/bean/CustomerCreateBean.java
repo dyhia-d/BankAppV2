b6fc03e711f2a9653b3f40b192e0cedbde84fb9f
Dyhia Dib <dyhia.dib@he-arc.ch>
2015-12-01 19:49:13 +0100

    Création de CustomerCreateBean qui appelle un service

---
+++ src/java/ch/hearc/ig/odi/customeraccount/bean/CustomerCreateBean.java
@@ -0,0 +1,52 @@
+package ch.hearc.ig.odi.customeraccount.bean;
+
+
+import ch.hearc.ig.odi.customeraccount.services.Services;
+import java.io.Serializable;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named (value="customer")
+@RequestScoped
+public class CustomerCreateBean implements Serializable {
+    private int number;
+    private String lastName;
+    private String firstName;
+    
+    
+    @Inject
+    Services services;
+    public CustomerCreateBean() {
+    }
+    
+    public void newCustomer(){
+        services.saveCustomer(this.number, this.firstName, this.lastName);
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+    
+}
